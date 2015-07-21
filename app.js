8e3a3f9cd9e0651888760057996d38a95f8007f4
unknown <erlin@EPIC20705.epic.com>
2015-07-21 10:33:54 -0500

    working copy no testing yet

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use('/users', users);
 var clients = []; // maintain list of connected clients
 var loggedinusers = {}; // maintain who is logged in
 var checkedinusers = {}; // maintain who is checked in
+var usernames = {}; // maintain list of usernames
 
 io.on('connection', function(socket){
   console.log('a user has connected', socket.id);
@@ -40,19 +41,36 @@ io.on('connection', function(socket){
   clients.push(socket);
   loggedinusers[socket.id] = false; // not logged in yet
   checkedinusers[socket.id] = false; // not checked in yet
+  // send current status of users to the new user
+  socket.emit('users-status', createUsersStatus(clients, loggedinusers, checkedinusers, usernames));
 
   // record disconnection
   socket.on('disconnect', function(){
     console.log('a user has disconnected');
     clients.splice(clients.indexOf(socket), 1);
+    delete loggedinusers[socket.id];
+    delete checkedinusers[socket.id];
+    delete usernames[socket.id];
+
+    io.emit('user-disconnected', socket.id);
   });
 
   // user attempted to log in
   socket.on('login', function(user){
+    console.log('users ', usernames);
+    // if user not logged in yet
     if(!loggedinusers[socket.id]){
-      io.emit('userloggedin', user);
+      io.emit('userloggedin', {username: user, userid: socket.id});
       loggedinusers[socket.id] = true;
+      usernames[socket.id] = user;
       console.log(user + ' has logged in');
+
+    }
+    else if(loggedinusers[socket.id] && usernames[socket.id] != user)
+    {
+      // user changed username then hit login
+      usernames[socket.id] = user;
+      io.emit('userchangedname', {userid: socket.id, newname: user});
     }
     else{
       console.log(user + ' has tried to log in but is already logged in');
@@ -64,7 +82,11 @@ io.on('connection', function(socket){
     if(!checkedinusers[socket.id]){
       checkedinusers[socket.id] = true;
       console.log(user + ' has checked in');
-      io.emit('usercheckedin', user);
+      io.emit('usercheckedin', socket.id);
+
+      // if all logged in users are also checked in
+      if(allUsersReady(clients, loggedinusers, checkedinusers))
+        io.emit('all-ready');
     }
     
   });
@@ -73,9 +95,47 @@ io.on('connection', function(socket){
   socket.on('checkout', function(user){
     checkedinusers[socket.id] = false;
     console.log(user + ' has checked out');
-    io.emit('usercheckedout', user);
+    io.emit('usercheckedout', socket.id);
   });
 });
+
+// returns an array of objects containing (for logged in users):
+// client id
+// username
+// checked in boolean
+createUsersStatus = function(clients, loggedinusers, checkedinusers, usernames)
+{
+  console.log('users logged in', Object.keys(loggedinusers));
+  var users = []
+  Object.keys(loggedinusers).forEach(function(userid) // iterate through user id's
+    {
+      if(loggedinusers[userid]){
+        var user = {};
+        user.userid = userid;
+        user.username = usernames[userid];
+        user.checkedin = checkedinusers[userid];
+        users.push(user);
+      }
+    }
+  );
+  return users;
+}
+
+allUsersReady = function(clients, loggedinusers, checkedinusers)
+{
+  console.log('checking if everyone is ready..');
+  console.log('checkedinusers: ', checkedinusers);
+  console.log('loggedinusers: ', loggedinusers);
+  var b = true;
+  Object.keys(loggedinusers).forEach(function(userid){
+    console.log(checkedinusers[userid], ' and ', loggedinusers[userid]);
+    if(!checkedinusers[userid] && loggedinusers[userid]){
+      b = false;
+    }
+      
+  });
+  return b;
+}
 
 /*END Socket.io stuff */
 
